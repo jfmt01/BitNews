92d0a0dfa68ba277250eec86295f293f51efba71
jfmt01 <jeison.jfmt@gmail.com>
2020-01-22 11:52:28 -0500

    Modal Noticias-Botón agregar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 function newCard(imagen, titulo, fuente, hora) {
 
   document.querySelector(`#news`).innerHTML += `
+  
   <div class="contNews">
+  <a  data-toggle="modal" data-target="#exampleModalLong">
     <div class = "imgNew">
-     <img src=${imagen} class="">
+    <img src=${imagen} class="">
     </div>
     <div class="textNew">
       <h5 class= "tituloNew">${titulo}</h5>
@@ -12,10 +14,12 @@ function newCard(imagen, titulo, fuente, hora) {
         <span class = "horaNew">${hora.substring(0, 10) + " " + hora.substring(12, 16)}</span>
       </div>
     </div>
-  </div>`
+    <a>  
+  </div>
+  `
 }
 
-function carousel(imagen, titulo, contenido, fuente, hora) {
+/* function carousel(imagen, titulo, contenido, fuente, hora) {
 
   document.querySelector(`#show`).innerHTML += `
     <div>
@@ -32,7 +36,7 @@ function carousel(imagen, titulo, contenido, fuente, hora) {
       </div>
     </div>`
 }
-
+ */
 var techNews = "hola";
 var url = 'https://newsapi.org/v2/top-headlines?country=co&category=technology&apiKey=1f06cf5bcd1643f9a433854dc526dce1';
 var request = new XMLHttpRequest();
@@ -45,7 +49,7 @@ request.onload = function () {
     newCard(techNews[i].urlToImage, techNews[i].title, techNews[i].source.name, techNews[i].publishedAt);
   }
   carousel(techNews[0].urlToImage, techNews[0].title, techNews[0].content, techNews[0].source.name, techNews[0].publishedAt);
-  document.querySelector(`body`).style.backgroundImage = `url(${techNews[0].urlToImage})`;
+  // document.querySelector(`body`).style.backgroundImage = 'linear-gradient(to left top, #02193B, #002B4C, #003C5A, #004F66, #03616F);';
   return techNews;
 }
 techNews = JSON.parse(request);
